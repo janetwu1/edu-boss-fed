053f6e2e02e4500a06b6b28ef2653cd17fc63bd9
janetwu1 <45478475+janetwu1@users.noreply.github.com>
2021-01-18 15:11:18 +0800

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,6 +5,13 @@ module.exports = {
     // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
     instances: 1,
     autorestart: true,
-    watch: false
+    watch: false,
+    max_memory_restart: '1G',
+    env: {
+      NODE_ENV: 'development'
+    },
+    env_production: {
+      NODE_ENV: 'production'
+    }
   }]
 }
