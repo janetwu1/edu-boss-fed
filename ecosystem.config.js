edf7ed8f488e0e11870f3b54e4a0a420cb6a69db
janetwu1 <45478475+janetwu1@users.noreply.github.com>
2022-12-12 11:17:21 +0800

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,6 +2,7 @@ module.exports = {
   apps: [{
     name: 'edu-boss-fed',
     script: '/usr/local/bin/npm',
+    args: 'start',
     // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
     instances: 1,
     autorestart: true,
